c9737fbc4ab5945f1c8cdd1fe5fae0e4e27a6913
shoeheart <jason@codebarn.com>
2020-12-06 14:17:31 -0600

    day 6 part 2

---
+++ app/Console/Commands/Advent/TwentyTwenty/Day6Part2.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace App\Console\Commands\Advent\TwentyTwenty;
+
+use App\Console\Commands\Advent\AdventBase;
+
+class Day6Part2 extends AdventBase {
+
+  protected $signature = "day6part2:perform";
+  protected $description = "Advent Of Code 2020 Day 6 Part 2";
+
+  public function handle() {
+    $groups = $this->_readGroupedInput();
+    //print_r($groups);
+    //echo "\n";
+
+    $sum = 0;
+    foreach ($groups as $group) {
+      //print_r($group);
+      //echo "\n";
+      $yesses = [];
+      foreach($group as $person) {
+        //print_r($person);
+        //echo "\n";
+        foreach(str_split($person) as $answer) {
+          if (isset($yesses[$answer])) {
+            $yesses[$answer]++;
+          } else {
+            $yesses[$answer] = 1;
+          }
+        }
+      }
+      $unanimous = 0;
+      foreach($yesses as $question => $numberOfPeopleAnsweringYes) {
+        if ($numberOfPeopleAnsweringYes == count($group)) {
+          $unanimous++;
+        }
+      }
+      //print_r($yesses);
+      //echo "\n";
+      //print_r($unanimous);
+      //echo "\n";
+      $sum += $unanimous;
+    }
+    echo "sum: " . $sum . "\n";
+  }
+}
