44603a601edff10c54479d6f6d6c60e938d36cb9
shoeheart <jason@codebarn.com>
2020-12-10 00:22:31 -0600

    day 10 parts 1 and 2

---
+++ app/Console/Commands/Advent/Year2020/Day10Part1.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Console\Commands\Advent\Year2020;
+
+use App\Console\Commands\Advent\AdventBase;
+
+class Day10Part1 extends AdventBase {
+
+  protected $signature = "year2020:day10part1";
+  protected $description = "Advent Of Code 2020 Day 10 Part 1";
+  protected $_lines = [];
+
+
+  public function handle() {
+    $this->_lines = $this->_readInput();
+    sort($this->_lines);
+    $this->_lines[] = end($this->_lines) + 3;
+
+    $diffCounts = [];
+
+    $previousJoltage = 0;
+    foreach($this->_lines as $index => $joltage) {
+      $diff = $joltage - $previousJoltage;
+      if (isset($diffCounts[$diff])) {
+        $diffCounts[$diff]++;
+      } else {
+        $diffCounts[$diff] = 1;
+      }
+      $previousJoltage = $joltage;
+      // echo "$index: $joltage\n";
+    }
+    // print_r($diffCounts);
+    echo "3*1 = " . ($diffCounts[1] * $diffCounts[3]) . "\n";
+  }
+}
---
+++ app/Console/Commands/Advent/Year2020/Day10Part2.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+
+namespace App\Console\Commands\Advent\Year2020;
+
+use App\Console\Commands\Advent\AdventBase;
+
+class Day10Part2 extends AdventBase {
+
+  protected $signature = "year2020:day10part2";
+  protected $description = "Advent Of Code 2020 Day 10 Part 2";
+  protected $_lines = [];
+  protected $_savedCounts = [];
+
+
+  // for each index in sorted array,
+  //   if index == last spot, return 1 (chain completed)
+  //   find subsequent indices whose value is +1, +2, or +3.
+  //   if list is empty, return 0
+  //   $sum = 0;
+  //   foreach of those,
+  //     $sum += completeChain($subsequentIndex)
+
+  //   return $sum
+
+  protected function _completeChain($startingIndex, $iteration, $depth) {
+    echo "iteration: $iteration, depth: $depth\n";
+    echo "completing $startingIndex: " . $this->_lines[$startingIndex] . "\n";
+    if ($startingIndex == count($this->_lines) - 1) {
+      echo "hit end\n";
+      return 1;
+    }
+
+    // powerset explosion that requires a cray
+    // if we don't save previously calculated results
+    if (isset($this->_savedCounts[$startingIndex])) {
+      echo "shunting $startingIndex\n";
+      return $this->_savedCounts[$startingIndex];
+    }
+
+    $currentJoltage = $this->_lines[$startingIndex];
+    $sum = 0;
+    $bother = true;
+    for ($i = $startingIndex + 1; $i < count($this->_lines) && $bother; $i++) {
+      $diff = ($this->_lines[$i] - $currentJoltage);
+      if ($diff > 0 && $diff <= 3 ) {
+        echo "processing $i: " . $this->_lines[$i] . "\n";
+        $sum += $this->_completeChain($i, $iteration + 1, $depth + 1);
+      } else {
+        echo "skipping $i: " . $this->_lines[$i] . "\n";
+        $bother = false;
+      }
+    }
+    echo "returning $sum\n";
+    $this->_savedCounts[$startingIndex] = $sum;
+    return $sum;
+  }
+
+  public function handle() {
+    $input = $this->_readInput();
+    sort($input);
+    $this->_lines = array_merge(
+      array(0),
+      $input,
+      array(end($input) + 3)
+    );
+    print_r($this->_lines);
+
+    echo "chains: " . $this->_completeChain(0, 1, 0);
+  }
+}
