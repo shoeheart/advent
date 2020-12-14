8b13fbb59f1ad7b8e2d22b7c6730fed8e9da0e11
shoeheart <jason@codebarn.com>
2020-12-13 20:05:16 -0600

    cleanup iteration count output

--- app/Console/Commands/Advent/Year2020/Day13Part2.php
+++ app/Console/Commands/Advent/Year2020/Day13Part2.php
@@ -39,11 +39,15 @@ class Day13Part2 extends AdventBase {
     // tiny data
     // $start = intdiv(1, $highestBusID) * $highestBusID - $this->_busses[$highestBusID];
     // real data
-    $start = intdiv(100000000000000, $highestBusID) * $highestBusID - $this->_busses[$highestBusID];
-    echo "$start, $highestBusID\n";
+    // $start = intdiv(100000000000000, $highestBusID) * $highestBusID - $this->_busses[$highestBusID];
+    // $start = intdiv(471793476184394, $highestBusID) * $highestBusID - $this->_busses[$highestBusID];
+    $start = intdiv(1, $highestBusID) * $highestBusID - $this->_busses[$highestBusID];
+    echo "starting at time $start, which is " . $this->_busses[$highestBusID] . " less than a multiple of $highestBusID\n";
     $incrementBy = $highestBusID;
     $highestFound = 0;
+    $iterations = 0;
     for($time = $start;; $time += $incrementBy) {
+      $iterations++;
       //echo "at time $time\n";
       $allValid = true;
       $i = 0;
@@ -52,7 +56,7 @@ class Day13Part2 extends AdventBase {
         // since we know it's a multiple
         if ($i > 0) {
           if($this->_isValid($time, $busIndex, $busID)) {
-            echo "found index $i for time $time, busID $busID, busIndex $busIndex\n";
+            // echo "found index $i for time $time, busID $busID, busIndex $busIndex\n";
             if ($i > $highestFound) {
               $incrementBy = $incrementBy * $busID;
               $highestFound = $i;
@@ -66,6 +70,7 @@ class Day13Part2 extends AdventBase {
       }
       if ($i == count($this->_busses)) {
         echo "answer: " . number_format($time) . "\n";
+        echo "in $iterations iterations\n";
         exit;
       }
     }
