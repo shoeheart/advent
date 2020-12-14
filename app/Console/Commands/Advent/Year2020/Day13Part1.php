22f4c1eee6e08134e505eb6240eaf5b3a2333158
shoeheart <jason@codebarn.com>
2020-12-13 19:56:08 -0600

    day 13

---
+++ app/Console/Commands/Advent/Year2020/Day13Part1.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace App\Console\Commands\Advent\Year2020;
+
+use App\Console\Commands\Advent\AdventBase;
+
+class Day13Part1 extends AdventBase {
+
+  protected $signature = "year2020:day13part1";
+  protected $description = "Advent Of Code 2020 Day 13 Part 1";
+  protected $_lines = [];
+
+  public function handle() {
+    $raw = $this->_readInput();
+    $earliestTimestamp = intval($raw[0]);
+    $busses = [];
+    foreach(explode(',', $raw[1]) as $bus) {
+      if ($bus =='x') continue;
+        $bus = intval($bus);
+        // $busses[$bus] = $earliestTimestamp - intdiv($earliestTimestamp, $bus);
+        // $busses[$bus] = $earliestTimestamp % $bus;
+        $busses[$bus] = $bus - ($earliestTimestamp % $bus);
+    }
+    echo $earliestTimestamp;
+    echo "\n";
+    print_r($busses);
+    echo "\n";
+    $soonestBus = -1;
+    foreach($busses as $bus => $wait) {
+      if ($soonestBus == -1) {
+        $soonestBus = $bus;
+      } else {
+        if ($wait < $busses[$soonestBus]) {
+          $soonestBus = $bus;
+        }
+      }
+    }
+    echo "answer: " . $soonestBus * $busses[$soonestBus] . "\n";
+  }
+}
---
+++ app/Console/Commands/Advent/Year2020/Day13Part2.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace App\Console\Commands\Advent\Year2020;
+
+use App\Console\Commands\Advent\AdventBase;
+
+class Day13Part2 extends AdventBase {
+
+  protected $signature = "year2020:day13part2";
+  protected $description = "Advent Of Code 2020 Day 13 Part 2";
+  protected $_busses = [];
+
+  protected function _isValid($time, $busIndex, $busID) {
+    //echo "testing: $time, $busIndex, $busID\n";
+    return (
+      ($busID == 'x') ||
+      // seems to work???
+      ((($time + $busIndex) % $busID) == 0)
+      // bad???
+      // (($busID - ($time % $busID)) == ($busIndex % $busID))
+    );
+  }
+
+  public function handle() {
+    $raw = $this->_readInput();
+    $i = 0;
+    foreach(explode(',', $raw[1]) as $busID) {
+      if ($busID != 'x') {
+        $this->_busses[$busID] = $i;
+      }
+      $i++;
+    }
+    krsort($this->_busses);
+    print_r($this->_busses);
+    echo "\n";
+    $highestBusID = array_key_first($this->_busses);
+    // sample data
+    //$start = intdiv(1060000, $highestBusID) * $highestBusID - $this->_busses[$highestBusID];
+    // tiny data
+    // $start = intdiv(1, $highestBusID) * $highestBusID - $this->_busses[$highestBusID];
+    // real data
+    $start = intdiv(100000000000000, $highestBusID) * $highestBusID - $this->_busses[$highestBusID];
+    echo "$start, $highestBusID\n";
+    $incrementBy = $highestBusID;
+    $highestFound = 0;
+    for($time = $start;; $time += $incrementBy) {
+      //echo "at time $time\n";
+      $allValid = true;
+      $i = 0;
+      foreach($this->_busses as $busID => $busIndex) {
+        // don't recalculate first one each time
+        // since we know it's a multiple
+        if ($i > 0) {
+          if($this->_isValid($time, $busIndex, $busID)) {
+            echo "found index $i for time $time, busID $busID, busIndex $busIndex\n";
+            if ($i > $highestFound) {
+              $incrementBy = $incrementBy * $busID;
+              $highestFound = $i;
+              echo "setting incrementBy to $incrementBy\n";
+            }
+          } else {
+            break;
+          }
+        }
+        $i++;
+      }
+      if ($i == count($this->_busses)) {
+        echo "answer: " . number_format($time) . "\n";
+        exit;
+      }
+    }
+  }
+}
