6cf3b207c8c59e92d25affcb7474d7e2c3d9bc9a
shoeheart <jason@codebarn.com>
2020-12-18 01:44:34 -0600

    day 18 part 1

--- app/Console/Commands/Advent/Year2020/Day18Part1.php
+++ app/Console/Commands/Advent/Year2020/Day18Part1.php
@@ -9,7 +9,63 @@ class Day18Part1 extends AdventBase {
   protected $signature = "year2020:day18part1";
   protected $description = "Advent Of Code 2020 Day 18 Part 1";
 
+  protected function _eval($expression) {
+    $expression = preg_replace('/\(/', '( ', $expression);
+    $expression = preg_replace('/\)/', ' )', $expression);
+    $tokens = explode(' ', $expression);
+    $depth = 0;
+    foreach($tokens as $token) {
+      switch ($token) {
+        case '(':
+          $depth++;
+          break;
+        case '+':
+          $currentOp[$depth] = '+';
+          break;
+        case '*':
+          $currentOp[$depth] = '*';
+          break;
+        case ')':
+          $token = $register[$depth];
+          unset($register[$depth]);
+          $depth--;
+        default:
+          if (isset($currentOp[$depth])) {
+            switch ($currentOp[$depth]) {
+              case "*":
+                $register[$depth] = $register[$depth] * intval($token);
+                break;
+              case "+":
+                $register[$depth] = $register[$depth] + intval($token);
+                break;
+            }
+            unset($currentOp[$depth]);
+          } else {
+            $register[$depth] = intval($token);
+          }
+      }
+    }
+    return $register[$depth];
+  }
+
   public function handle() {
     $lines = $this->_readInput();
+    $problems = [];
+
+    foreach($lines as $line) {
+      $problems[] = $line;
+    }
+
+    //print_r($problems);
+    //echo "\n";
+
+    $sum = 0;
+    foreach($problems as $problem) {
+      echo "expression: " . $problem . "\n";
+      $result = $this->_eval($problem);
+      $sum += $result;
+      echo "  result: $result\n";
+    }
+    echo "sum: $sum\n";
   }
 }
