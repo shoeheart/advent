1201fc2fe04c3c061f00012b452ad114f1ab7d00
shoeheart <jason@codebarn.com>
2020-12-25 23:22:25 -0600

    day 19 part 2 and day 20 part 2 wip

--- app/Console/Commands/Advent/Year2020/Day19Part2.php
+++ app/Console/Commands/Advent/Year2020/Day19Part2.php
@@ -8,8 +8,262 @@ class Day19Part2 extends AdventBase {
 
   protected $signature = "year2020:day19part2";
   protected $description = "Advent Of Code 2020 Day 19 Part 2";
+  protected $stringRules = [];
+  protected $finiteCompositeRules = [];
+  protected $recursiveRules = [];
+  protected $images = [];
+
+
+  // after collapsing rules, we are left with
+  // 0: 8 11
+  // 8: 42 | 42 8
+  // 11: 42 31 | 42 11 31
+  //
+  // result is however may 31s appear at end, must equal or great
+  //
+  // so for a message to match, it must start with one or more 42s
+  // followed by again one or more 42s, and finally a single 31
+  //
+  // AND however many 31s appear at end, must greater number of
+  // 42s at beginning in order to match.
+  //
+
+  protected function _cartesian($a1, $a2) {
+    //echo "carting\n";
+    //print_r($a1);
+    //print_r($a2);
+    if (empty($a1)) {
+      return $a2;
+    }
+    //echo "a1 not empty\n";
+    $result = [];
+    foreach ($a1 as $a1v) {
+      foreach($a2 as $a2v) {
+        //echo "combining $a1v and $a2v\n";
+        $result[] = $a1v . $a2v;
+        //print_r($result);
+      }
+    }
+    return $result;
+  }
 
   public function handle() {
     $lines = $this->_readInput();
+    $mode = 'rules';
+    foreach($lines as $line) {
+      if (empty($line)) {
+        $mode = 'images';
+        continue;
+      }
+      if ($mode == 'rules') {
+        $split = explode(':', $line);
+        $ruleNumber = $split[0];
+        $ruleDefinition = trim($split[1]);
+        // skip recursive rules.  handled in code
+        if ($ruleNumber == 0 || $ruleNumber == 8 || $ruleNumber == 11) {
+          continue;
+        }
+        if (strpos($ruleDefinition, '"') === false) {
+          $subrules = explode('|', trim($ruleDefinition));
+          $rule = array();
+
+          foreach($subrules as $subrule) {
+            $subruleset = [];
+            $tokens = explode(' ', trim($subrule));
+            foreach($tokens as $token) {
+              $subruleset[] = $token;
+            }
+            $rule[] = $subruleset;
+          }
+          $this->finiteCompositeRules[$ruleNumber] = $rule;
+        } else {
+          // only single character rules in input so grab the char
+          // but keep it as array of possible values since we
+          // need each finiteCompositeRule to decompose down to
+          // an array of possible string matches
+          $this->stringRules[$ruleNumber] = array($ruleDefinition[1]);
+        }
+      } else {
+        $this->images[] = $line;
+      }
+    }
+    // 0: 8 11
+    // 8: 42 | 42 8'
+    // 11: 42 31 | 42 11 31'
+    $this->recursiveRules[0] = array(array(8, 11));
+    $this->recursiveRules[8] = array(array(42), array(42, 8));
+    $this->recursiveRules[11] = array(array(42, 31), array(42, 11, 31));
+
+    // echo "string rules:\n";
+    // print_r($this->stringRules);
+    // echo "finite composite rules:\n";
+    // print_r($this->finiteCompositeRules);
+    // echo "recursive rules:\n";
+    // print_r($this->recursiveRules);
+
+    while (count($this->finiteCompositeRules)) {
+      foreach($this->finiteCompositeRules as $ruleNumber => $subrulesets) {
+        $stringRule = [];
+        //echo "checking $ruleNumber\n";
+        $allStrings = true;
+        foreach($subrulesets as $i => $subruleset) {
+          $subStringRule = [];
+          if (! $allStrings) {
+            //echo "breaking subrulesets\n";
+            break;
+          }
+          foreach($subruleset as $j => $subrule) {
+            //echo "checking sub $subrule\n";
+            if (! isset($this->stringRules[$subrule])) {
+              //echo "breaking subrule\n";
+              $allStrings = false;
+              break;
+            } else {
+              //echo "carting $ruleNumber $subrule\n";
+              //print_r($subStringRule);
+              //print_r($this->stringRules[$subrule]);
+              $subStringRule =
+                $this->_cartesian(
+                  $subStringRule,
+                  $this->stringRules[$subrule]
+                );
+              //echo "subStringRule\n";
+              //print_r($subStringRule);
+            }
+          }
+          $stringRule = array_merge($stringRule, $subStringRule);
+        }
+        if ($allStrings) {
+          //echo "rule $ruleNumber is composed of all strings\n";
+          //print_r($stringRule);
+          //echo "\n";
+          $this->stringRules[$ruleNumber] = $stringRule;
+          unset($this->finiteCompositeRules[$ruleNumber]);
+        }
+      }
+    }
+
+    //echo "all string rules:\n";
+    //print_r($this->stringRules);
+
+    $usedStringRules = [];
+    foreach ($this->recursiveRules as $ruleNumber => $subrulesets) {
+      foreach ($subrulesets as $subrules) {
+        foreach ($subrules as $subrule) {
+          $usedStringRules[] = $subrule;
+        }
+      }
+    }
+
+    foreach ($this->stringRules as $ruleNumber => $_) {
+      if (! (in_array($ruleNumber, $usedStringRules))) {
+        unset($this->stringRules[$ruleNumber]);
+      }
+    }
+
+    // echo "Using rules:\n";
+    // echo "recursive rules:\n";
+    // print_r($this->recursiveRules);
+    // //echo "finite composite rules:\n";
+    // //print_r($this->finiteCompositeRules);
+    // echo "string rules:\n";
+    // print_r($this->stringRules);
+
+    // echo "31 initial count: " . count($this->stringRules[31]) . "\n";
+    $this->stringRules[31] = array_unique($this->stringRules[31]);
+    //echo "31 unique count: " . count($this->stringRules[31]) . "\n";
+    sort($this->stringRules[31]);
+    //echo "31 sorted count: " . count($this->stringRules[31]) . "\n";
+
+    //echo "42 initial count: " . count($this->stringRules[42]) . "\n";
+    $this->stringRules[42] = array_unique($this->stringRules[42]);
+    //echo "42 unique count: " . count($this->stringRules[42]) . "\n";
+    sort($this->stringRules[42]);
+    //echo "42 sorted count: " . count($this->stringRules[42]) . "\n";
+
+    // echo "overlap between 31 and 42\n";
+    // print_r(
+    //   array_intersect(
+    //     $this->stringRules[31],
+    //     $this->stringRules[42]
+    //   )
+    // );
+
+    echo "string rules:\n";
+    print_r($this->stringRules);
+
+    // exit -1;
+
+    // problem here is we need to consume all the 31s at end of string
+    // before starting to look for 42s and count them
+    // so we can match at least one more 42 in remainder
+    // than we found 31s at end
+    $endInThirtyOne = [];
+    foreach($this->images as $image) {
+      list($remainder, $thirtyOnesFound) = $this->_consumeThirtyOnes($image, 0);
+      if ($thirtyOnesFound > 0) {
+        $endInThirtyOne[] = array($remainder, $thirtyOnesFound);
+      } else {
+        "no 31s found at end of $image\n";
+      }
+    }
+
+    //echo "out of " . count($endInThirtyOne) . " candidates to match 42+: " . count($endInThirtyOne) . "\n";
+    //print_r($endInThirtyOne);
+
+    $startWithFortyTwoPlusOneNoRemainder = [];
+    foreach($endInThirtyOne as $imageAndThirtyOneCount) {
+      $toMatch = $imageAndThirtyOneCount[0];
+      $needed = $imageAndThirtyOneCount[1] + 1;
+      $found = $this->_matchFortyTwoPlusOneNoRemainder($toMatch);
+      //echo "found $found vs needed $needed\n";
+      if ($found >= $needed) {
+        $startWithFortyTwoPlusOneNoRemainder[] = $imageAndThirtyOneCount[0];
+      } else {
+        echo "NOT ENOUGH 42 MATCH: " . $imageAndThirtyOneCount[0] . "\n";
+      }
+    }
+
+    echo "final match count: " . count($startWithFortyTwoPlusOneNoRemainder) . "\n";
+  }
+
+  protected function _consumeThirtyOnes($image, $thirtyOnesFound = 0) {
+    foreach($this->stringRules[31] as $thirtyOne) {
+      $expression = '/^(.*)(' . $thirtyOne . ')$/';
+      //echo "$expression\n";
+      if (preg_match($expression, $image, $matches)) {
+        //echo "$image ends in 31\n";
+        //echo "matched " . $matches[2] . "\n";
+        //echo "remainder " . $matches[1] . "\n";
+        return $this->_consumeThirtyOnes($matches[1], $thirtyOnesFound + 1);
+      }
+    }
+    //echo "returning unchanged ($image, $thirtyOnesFound)\n";
+    return array($image, $thirtyOnesFound);
+  }
+
+  protected function _matchFortyTwoPlusOneNoRemainder($image, $foundWithNoRemainder = 0) {
+
+    //echo "_matchFortyTwoPlusOneNoRemainder: $image\n";
+    if (strlen($image) == 0) {
+      //echo "ran out in good way!\n";
+      return $foundWithNoRemainder;
+    }
+    foreach($this->stringRules[42] as $fortyTwo) {
+      $expression = '/^(' . $fortyTwo . ')(.*)$/';
+      //echo "testing for 42: $image\n";
+      //echo "$expression\n";
+      if (preg_match($expression, $image, $matches)) {
+        //echo "matched " . $matches[1] . "\n";
+        //echo "remainder " . $matches[2] . "\n";
+        //print_r($matches);
+        // if match resulted in remainder of length zero
+        // or if remainder also matches 42, we match
+        // and we can stop searching other forks
+        return $this->_matchFortyTwoPlusOneNoRemainder($matches[2], $foundWithNoRemainder + 1);
+      }
+    }
+    //echo "no 42s matched remainder $image\n";
+    return 0;
   }
 }
--- app/Console/Commands/Advent/Year2020/Day20Part2.php
+++ app/Console/Commands/Advent/Year2020/Day20Part2.php
@@ -8,8 +8,201 @@ class Day20Part2 extends AdventBase {
 
   protected $signature = "year2020:day20part2";
   protected $description = "Advent Of Code 2020 Day 20 Part 2";
+  protected $_tiles = [];
+
+  protected function _rotate90($mat) {
+    $height = count($mat);
+    $width = strlen($mat[0]);
+    $mat90 = array();
+
+    for ($x = 0; $x < $width; $x++) {
+        $line = '';
+        for ($y = 0; $y < $height; $y++) {
+            $line = $line . $mat[$height - $y - 1][$x];
+        }
+        $mat90[] = $line;
+    }
+
+    return $mat90;
+  }
+
+  protected function _flipTopToBottom($mat) {
+    return array_reverse($mat);
+  }
+
+  protected function _flipLeftToRight($mat) {
+    $height = count($mat);
+    $matFlip = array();
+
+    for ($y = 0; $y < $height; $y++) {
+      $matFlip[] = strrev($mat[$y]);
+    }
+
+    return $matFlip;
+  }
+
+  protected function _permutations($tile) {
+    $permutations = [];
+    $permutations[$tile[0]] = $tile;
+    $tile = $this->_rotate90($tile);
+    $permutations[$tile[0]] = $tile;
+    $tile = $this->_rotate90($tile);
+    $permutations[$tile[0]] = $tile;
+    $tile = $this->_rotate90($tile);
+    $permutations[$tile[0]] = $tile;
+    $tile = $this->_flipTopToBottom($tile);
+    $permutations[$tile[0]] = $tile;
+    $tile = $this->_rotate90($tile);
+    $permutations[$tile[0]] = $tile;
+    $tile = $this->_rotate90($tile);
+    $permutations[$tile[0]] = $tile;
+    $tile = $this->_rotate90($tile);
+    $permutations[$tile[0]] = $tile;
+
+    return $permutations;
+  }
+
+  protected function _attach($tile1ID, $tile2ID) {
+    $edges1 = array_keys($this->_tiles[$tile1ID]['permutations']);
+    $edges2 = array_keys($this->_tiles[$tile2ID]['permutations']);
+    // echo "    comparing tile $tile1ID edges\n";
+    // print_r($edges1);
+    // echo "    against tile $tile2ID edges\n";
+    // print_r($edges2);
+    // echo "\n";
+
+    foreach($edges1 as $i1 => $e1) {
+      foreach($edges2 as $i2 => $e2) {
+        if ($e1 == $e2) {
+          // lock these tiles to the orientation that matched
+          // if(isset($this->_tiles[$tile1ID]['oriented'])) {
+          //   echo "      $tile1ID already oriented to " .
+          //     $this->_tiles[$tile1ID]['oriented'] .
+          //     " but trying to reorient to $e1\n";
+          // } else {
+          //   echo "      orienting $tile1ID to $e1\n";
+          //   $this->_tiles[$tile1ID]['oriented'] = $e1;
+          // }
+
+          if (!array_key_exists($e1, $this->_tiles[$tile1ID]['connections'])) {
+            $this->_tiles[$tile1ID]['connections'][$e1] = $tile2ID;
+          }
+          //if (!array_key_exists($e2, $this->_tiles[$tile2ID]['connections'])) {
+            //$this->_tiles[$tile2ID]['connections'][$e2] = $tile1ID;
+          //}
+
+          // if(isset($this->_tiles[$tile2ID]['oriented'])) {
+          //   echo "      $tile2ID already oriented to " .
+          //     $this->_tiles[$tile2ID]['oriented'] .
+          //     " but trying to reorient to $e2\n";
+          // } else {
+          //   echo "      orienting $tile2ID to $e2\n";
+          //   $this->_tiles[$tile2ID]['oriented'] = $attach[0];
+          // }
+
+          // echo "      matched $tile1ID to $tile2ID\n";
+          // print_r($this->_tiles[$tile1ID]['permutations'][$e1]);
+          // $attach = $this->_flipTopToBottom($this->_tiles[$tile1ID]['permutations'][$e2]);
+          // print_r($attach);
+          // echo "\n";
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 
   public function handle() {
     $lines = $this->_readInput();
+    foreach($lines as $line) {
+      if (empty($line)) {
+        $this->_tiles[$tileID]['permutations'] = $this->_permutations($tile);
+        $this->_tiles[$tileID]['connections'] = [];
+      } elseif (preg_match('/Tile (\d+):/', $line, $matches)) {
+        $tileID = $matches[1];
+        $tile = [];
+      } else {
+        $tile[] = $line;
+      }
+    }
+    $this->_tiles[$tileID]['permutations'] = $this->_permutations($tile);
+    $this->_tiles[$tileID]['connections'] = [];
+
+    //print_r($this->_tiles);
+    //echo "\n";
+
+    $corners = [];
+    foreach($this->_tiles as $possibleCornerID => $_) {
+      //echo "testing $possibleCornerID\n";
+      $attachments = 0;
+      foreach($this->_tiles as $possibleAttachmentID => $_) {
+        //echo "  against $possibleAttachmentID\n";
+        // can't attach ourself to ourself
+        if ($possibleCornerID == $possibleAttachmentID) {
+          //echo "    skipping self\n";
+          continue;
+        }
+        if ($this->_attach($possibleCornerID, $possibleAttachmentID)) {
+          //echo "  found match!!!\n";
+          $attachments++;
+        }
+      }
+      if ($attachments == 2) {
+        $corners[] = $possibleCornerID;
+      } elseif ($attachments == 3) {
+        $borders[] = $possibleCornerID;
+      } else {
+        if ($attachments != 4) {
+          echo "found $attachments to tile $possibleCornerID\n";
+        }
+        $middles[] = $possibleCornerID;
+      }
+    }
+
+    echo "tiles after finding corners\n";
+    print_r($this->_tiles);
+    echo "\n";
+
+    print_r($corners);
+    echo "\n";
+    $product = 1;
+    foreach($corners as $corner) {
+      $product = $product * $corner;
+      echo "corner tile $corner:\n";
+      print_r($this->_tiles[$corner]);
+      echo "\n";
+    }
+    echo "product: $product\n";
+    echo "borders: " . count($borders) . "\n";
+    echo "middles: " . count($middles) . "\n";
+    echo "total: " . count($this->_tiles) . "\n";
+
+    // take first corner and rotate so connection are to right and down
+    // do this by finding permutation that matches.  The next permutation in
+    // the list will be the second connection, and is rotated 90 degrees right from
+    // first connection's orientation and hence rotating first orientation
+    // 90 degrees right twice will make this the top left corner
+    $topLeft = $this->_tiles[$corners[0]];
+    $boardTopLeft = array();
+    foreach ($topLeft['connections'] as $connectionEdge => $connectionID) {
+      $found = false;
+      if (array_key_exists($permutationEdge, $topLeft['permutations'])) {
+        // first one found is bottom edge
+        if (!$found) {
+          $mat = $this->_rotate90($permutationLines);
+          $mat = $this->_rotate90($mat);
+          $board[] = [];
+          $board[0][] = array(
+            'down' => $permutationEdge,
+            'lines' => $mat,
+          );
+          $found = true;
+        } else {
+          $board[0][0]['right'] = $permutationEdge;
+        }
+      }
+    }
+
+    print_r($board);
   }
 }
